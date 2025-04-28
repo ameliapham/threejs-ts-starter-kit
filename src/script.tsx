a50617c02ebb01c9321ccf420fa923188bd427ef
Huong Pham <59421719+ameliapham@users.noreply.github.com>
2025-04-28 22:55:07 +0200

    Setup

--- src/script.tsx
+++ src/script.tsx
@@ -1,4 +1,51 @@
-import * as THREE from 'three'
-import gsap from 'gsap'
+import * as THREE from "three";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 console.log("Hello, Three.js with TypeScript!");
+
+// --- Canvas Setup ---
+const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
+console.log("hello canvas")
+
+// --- Scene Setup ---
+const scene = new THREE.Scene();
+console.log("hello scene")
+
+// --- Axes Helper ---
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
+// --- Size Setup ---
+const size = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+// --- Camera Setup ---
+const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
+scene.add(camera)
+camera.position.z = 3
+
+// --- Resize ---
+window.addEventListener("resize", () => {
+    camera.aspect = size.width/size.height
+    camera.updateProjectionMatrix()
+    renderer.setSize(size.width, size.height)
+})
+
+// --- Controls ---
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+// --- Renderer Setup ---
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(size.width, size.height);
+
+// --- Render Loop ---
+function animate(){
+    controls.update()
+  
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(animate)
+}
+animate()
